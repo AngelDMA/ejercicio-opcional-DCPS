17af87c397ece0d316afc6c3c21e6c2a6d496897
Angel David Machado <amachadoa@unal.edu.co>
2019-08-21 13:31:41 -0500

    se agrega vista a registar

--- resources/views/vistas/registrar.blade.php
+++ resources/views/vistas/registrar.blade.php
@@ -1,1 +1,22 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="nombre">Nompre</label>
+                <input id="nombre" class="form-control" type="text" name="nombre">
+            </div>
+
+            <div class="form-group">
+                <label for="cedula">Cedula</label>
+                <input id="cedula" class="form-control" type="number" name="cedula">
+            </div>
+
+            <div class="form-group">
+                <label for="placa">Placa</label>
+                <input id="placa" class="form-control" type="text" name="placa">
+            </div>
+        </form>
+    </div>
+@endsection
