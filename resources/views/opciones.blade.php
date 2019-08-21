196d6382332353e9ad1c9aff3b86af52ce5e7663
Angel David Machado <amachadoa@unal.edu.co>
2019-08-21 12:56:34 -0500

    se actualian enlaces

--- resources/views/opciones.blade.php
+++ resources/views/opciones.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="content">
         <div class="links">
-            <a href="https://laravel.com/docs">Registrar vehículo</a>
-            <a href="https://laracasts.com">Litar vehículos</a>
-            <a href="https://laravel-news.com">Estadísticas vehículo</a>
+            <a href="/registrar">Registrar vehículo</a>
+            <a href="/listar">Listar vehículos</a>
+            <a href="/estadistica">Estadísticas vehículo</a>
     </div>
 @endsection
