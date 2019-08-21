35b8aef7775ac1ad519a80f0662dac0e0c09d82b
Angel David Machado <amachadoa@unal.edu.co>
2019-08-21 13:02:01 -0500

    Se actualiza el controlador Persona

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PersonaController extends Controller
 {
-    //
+    public function registrarIndex()
+    {
+        return view('vistas.registrar');
+    }
+
+    public function listarIndex()
+    {
+        return view('vistas.listar');
+    }
+
+    public function estadisticaIndex()
+    {
+        return view('vistas.estadistica');
+    }
+
 }
